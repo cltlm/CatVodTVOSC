0726607d6e61ca271895165e74c566669db3f245
cltlm <50360202+cltlm@users.noreply.github.com>
2022-10-08 13:49:40 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -79,7 +79,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://dl.zici.fr/1680760144-81/.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
